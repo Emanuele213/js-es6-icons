7d12ea4caa83a5e816769518eccaa8cd6f69c233
Emanuele213 <vittorius213@gmail.com>
2022-11-10 13:11:55 +0100

    corrected with the tutor

--- js/script.js
+++ js/script.js
@@ -113,19 +113,76 @@ const arrIcons = [
 	}
 ];
 
-const eleContainer = document.querySelector('.main-container');
-const eleCard = document.createElement('div');
-eleCard.classList.add('card');
-const eleType = document.querySelector('#type');
-const eleNewType = eleType.value;
+const eleIconsContainer = document.querySelector('#icons-container');
+const eleSelect = document.querySelector('#type-filter');
 
-arrIcons.forEach ((eleIcons) => {
-    eleContainer.append(eleCard);
+// stampare le card delle icone
+populateSelect(arrIcons, eleSelect);
+randomizeColors(arrIcons);
+renderIcons(arrIcons, eleIconsContainer);
+eleSelect.addEventListener('change', filterIcons);
 
-    eleCard.innerHTML += `
-    <div class="icon">
-        <i class="${eleIcons.prefix}solid ${eleIcons.prefix}${eleIcons.name} ${eleIcons.color}"></i>
-        <div class="title-icon">${eleIcons.name}</div>
-    </div>`;
-});
 
+// FUNCTIONS
+
+function renderIcons(arrData, eleContainer) {
+	eleContainer.innerHTML = '';
+	arrData.forEach(objIcon => eleContainer.innerHTML += generateCard(objIcon));
+
+	// for (let i = 0; i < arrData.length; i++) {
+	// 	const objIcon = arrData[i];
+	// 	eleContainer.innerHTML += generateCard(objIcon);
+	// }
+}
+
+function generateCard(objData) {
+	return `
+		<div class="box">
+			<i class="${objData.family} ${objData.prefix}${objData.name}" style="color:${objData.color}"></i>
+			<div class="title">${objData.name}</div>
+		</div>
+	`;
+}
+
+function populateSelect(arrData, eleSelect) {
+	// estrapolare i type dall'array dei dati
+	const arrTypes = [];
+	arrData.forEach(objIcon => arrTypes.includes(objIcon.type) ? '' : arrTypes.push(objIcon.type));
+
+	console.log(arrTypes);
+
+	// aggiungere le options per ciscun tipo
+	arrTypes.forEach(type => eleSelect.innerHTML += `<option value="${type}">${type}</option>`);
+}
+
+function filterIcons() {
+	const selectedType = this.value; // qui avrebbe funzionato anche eleSelect essendo in scope, ma ci sono casi in cui la variabile non e' in scope, il this funziona sempre
+
+	// filtrare array
+	if (selectedType !== '') {
+		arrIconsFiltered = arrIcons.filter(objIcon => objIcon.type === selectedType)
+	} else {
+		arrIconsFiltered = arrIcons;
+	}
+	console.log(arrIconsFiltered);
+
+	// renderizzare array delle icone filtrate
+	renderIcons(arrIconsFiltered, eleIconsContainer);
+}
+
+function randomizeColors(arrIcons) {
+	arrIcons.forEach(objIcon => objIcon.color = getRandomColor());
+}
+
+function getRandomColor() {
+	const acceptableChars = '0123456789ABCDEF';
+	let color = '#'
+	for (let i = 0; i < 6; i++) {
+		color += acceptableChars[getRandomInteger(0, 15)];
+	}
+	return color;
+}
+
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
